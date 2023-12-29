d4ded8b77091d6531b1ffae252c13a9bb1ba761e
Paul <Paul_L_Preston@hotmail.com>
2023-12-29 18:24:47 +1000

    Allow summing strings

--- src/main/java/com/bedrockk/molang/ast/binaryop/PlusExpression.java
+++ src/main/java/com/bedrockk/molang/ast/binaryop/PlusExpression.java
@@ -2,6 +2,7 @@ package com.bedrockk.molang.ast.binaryop;
 
 import com.bedrockk.molang.Expression;
 import com.bedrockk.molang.ast.BinaryOpExpression;
+import com.bedrockk.molang.ast.StringExpression;
 import com.bedrockk.molang.runtime.MoLangEnvironment;
 import com.bedrockk.molang.runtime.MoScope;
 import com.bedrockk.molang.runtime.value.DoubleValue;
@@ -20,6 +21,10 @@ public class PlusExpression extends BinaryOpExpression {
 
     @Override
     public MoValue evaluate(MoScope scope, MoLangEnvironment environment) {
-        return new DoubleValue(left.evaluate(scope, environment).asDouble() + right.evaluate(scope, environment).asDouble());
+        if (left instanceof StringExpression || right instanceof StringExpression) {
+            return new DoubleValue(left.evaluate(scope, environment).asString() + right.evaluate(scope, environment).asString());
+        } else {
+            return new DoubleValue(left.evaluate(scope, environment).asDouble() + right.evaluate(scope, environment).asDouble());
+        }
     }
 }
